1d73df5d7c64debca9b2c697f650abc160041dcb
Zack <zack.learns@gmail.com>
2018-05-03 12:06:27 +0800

    implemented income input and set it to state

--- src/Component/TaxCalculator.js
+++ src/Component/TaxCalculator.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react'
 import Income from './Income'
-import './TaxCalculator.css'
 
 class TaxCalculator extends Component {
+
+  state = {
+    income: 0,
+    taxPayable: 0
+  }
+
+  calculate = () => {
+    const {income} = this.state
+
+    this.setState({
+      income: income
+    })
+
+  }
+
+  updateIncome = (name, value) => {
+    this.setState({
+      [name]: value
+    })
+  }
+
 
   render () {
     return (
       <div className="tax-calculator">
-        <Income/>
+        <Income
+          name="income"
+          updateIncome={this.updateIncome}
+        />
+        <button onClick={this.calculate}>Calculate</button>
+        <div className="taxPayable">Tax: {this.state.income}</div>
       </div>
     )
   }
 }
 
-export default TaxCalculator
-// Gary's test
+export default TaxCalculator// Gary's test
