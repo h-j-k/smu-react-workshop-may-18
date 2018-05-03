f314276948dbaf1b082b916d7ea4e753f4c7395b
DESKTOP-660JSU5\nigel <nigelsmk@gmail.com>
2018-05-03 12:00:46 +0800

    Revert "initial tax payable test commit"
    This reverts commit d6ecf603f5484f909dc04206ad1ce9e17cdad0a6.

--- src/Component/TaxCalculator.js
+++ src/Component/TaxCalculator.js
@@ -3,7 +3,7 @@ import Income from './Income'
 import './TaxCalculator.css'
 
 class TaxCalculator extends Component {
-  //calculate tax payable
+
   render () {
     return (
       <div className="tax-calculator">
