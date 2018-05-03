bc94b6e74bc7086787bcc12c274a1ba1b04eb87c
Gary CHUA Rongjie <rongkie93@gmail.com>
2018-05-03 11:59:22 +0800

    added tax-relief radio buttons for income spouse parent ns relief

--- src/Component/Income.js
+++ src/Component/Income.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 class Income extends React.Component {
 
-  render () {
+
+  
+
+
+
+  render() {
     return (
       <div className="income-section">
         <table>
-          <thead>
+          <th>
+            <td colSpan="2">Income</td>
+          </th>
           <tr>
-            <th colSpan="2">Awesome Income Tax Calculator</th>
+            <td>Employment Income</td>
+            <td><input type='text'/></td>
           </tr>
-          </thead>
-          <tbody>
           <tr>
-            <td>Salary Income</td>
-            <td><input/></td>
+            <td>Earned Income relief</td>
+            <td><input type='radio' value="55"/>Below 55</td>
+            <td><input type='radio' value="59"/>55 to 59</td>
+            <td><input type='radio' value="60"/>60 and Above</td>
           </tr>
-          </tbody>
+          <tr>
+            <td>Spouse relief</td>
+            <td><input type='radio' value="Y"/>Yes</td>
+            <td><input type='radio' value="N"/>No</td>
+          </tr>
+
+           <tr>
+            <td>Parent relief</td>
+            <td><input type='radio' value='' />Stay with dependent</td>
+            <td><input type='radio' value=''/>Does not stay with dependent</td>
+          </tr>
+
+           <tr>
+            <td>NS relief</td>
+            <td><input type='radio' value="Y"/>Yes</td>
+            <td><input type='radio' value="N"/>No</td>
+          </tr>
         </table>
       </div>
     )
